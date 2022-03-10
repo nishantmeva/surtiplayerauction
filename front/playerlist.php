7b1652a5d2889ce19cc6e3d399c975f891788cc1
Nishant Mevawala <nishantmeva@gmail.com>
2022-03-10 20:23:39 +0530

    player list

--- front/playerlist.php
+++ front/playerlist.php
@@ -1,8 +1,9 @@
 <?php 
 require_once('header.php'); 
 
-$UnsoldPlayer = getUnsoldPlayer();
-//echo "<pre>"; print_r($UnsoldPlayer); exit;
+$playerList = getPlayerList();
+$teamList = getTeams();
+// echo "<pre>"; print_r($playerList); exit;
 ?>
 
 <!-- Page Content-->
@@ -11,18 +12,18 @@ $UnsoldPlayer = getUnsoldPlayer();
         <div class="row teamplayerPage">
             <div class="col-sm-12 col-md-12">
                 <center>
-                <h2>Unsold Players</h2>
-                <p>No of Players: <?= count($UnsoldPlayer); ?></p>
+                <h2>Player List</h2>
+                <p>No of Players: <?= count($playerList); ?></p>
                 </center>
             </div>
             <table class="table">
                 <thead class="thead-dark">
                     <tr>
-                    <th scope="col">Form No</th>
-                    <th scope="col">Member No</th>
+                    <th scope="col" width="5%">Form No</th>
+                    <th scope="col" width="5%">Member No</th>
                     <th scope="col">Photo</th>
-                    <th scope="col">Name</th>
-                    <th scope="col">Previous Team</th>
+                    <th scope="col" width="25%">Name</th>
+                    <th scope="col" width="15%">Previous Team</th>
                     <th scope="col">Batting Style</th>
                     <th scope="col">Bowling Style</th>
                     <th scope="col">Is wicket keeper</th>
@@ -30,12 +31,28 @@ $UnsoldPlayer = getUnsoldPlayer();
                     </tr>
                 </thead>
                 <tbody>
-                    <?php foreach($UnsoldPlayer as $data){ ?>
+                    <?php foreach($playerList as $data){ ?>
                         <tr title="<?= $data['id']; ?>">
                             <th scope="row"><?= $data['formno']; ?></th>
                             <td><?= $data['memberno']; ?></td>
                             <td><img width="50px" src="../common_uploads/players/<?= $data['photo']; ?>"/></td>
-                            <td><?= strtoupper($data['name']); ?></td>
+                            <td>
+                                <?= strtoupper($data['name']); ?></br>
+                                <?php if(!empty($teamList[$data['team_id']])){ 
+                                    $teamData = $teamList[$data['team_id']];
+                                    $teamName = $teamData['name'];
+                                    echo "<small><b>".$teamName."</b></small>";
+                                } ?>
+                                <?php if($data['is_captain']){
+                                    echo " | <small><b>Captain</b></small>";
+                                }?>
+                                <?php if($data['sold_status']){
+                                    echo " | <small><b>".$data['sold_status']."</b></small>";
+                                }?>
+                                <?php if(!empty($data['sold_points'])){
+                                    echo " | <small><b>".$data['sold_points']."</b></small>";
+                                }?>
+                            </td>
                             <td><?= strtoupper($data['previous_team']); ?></td>
                             <td><?= strtoupper($data['batting_style']); ?></td>
                             <td><?= strtoupper($data['bowling_style']); ?></td>
